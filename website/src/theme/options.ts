41bc401bef378ce552f4d6af70f8bd8da42a0327
Tomáš Čarnecký <tomc@caurea.org>
2026-04-09 10:34:15 +0200

    Use moduleResolution=bundler (#487)

--- website/src/theme/options.ts
+++ website/src/theme/options.ts
@@ -1,14 +1,13 @@
 import shadows from "./shadows";
 import * as colors from "./colors";
-import { FontStyleOptions } from "@mui/material/styles/createTypography";
 import { Shadows, ThemeOptions } from "@mui/material";
 
-const fontStyleOptions: FontStyleOptions = {
+const fontStyleOptions = {
   fontWeightLight: 300,
   fontWeightRegular: 400,
   fontWeightMedium: 500,
   fontWeightBold: 700,
-};
+} as const;
 
 export default {
   palette: {
