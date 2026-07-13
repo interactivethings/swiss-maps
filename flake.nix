c2ed47f90fc8effd84ae9c681b31affe80b94fec
Tomas Carnecky <tomc@caurea.org>
2026-07-13 10:26:16 +0200

    Stop using flake-utils

--- flake.nix
+++ flake.nix
@@ -1,26 +1,28 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    systems.url = "github:nix-systems/default";
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+    {
+      nixpkgs,
+      systems,
+      ...
+    }:
+    let
+      forAllSystems =
+        function: nixpkgs.lib.genAttrs (import systems) (system: function nixpkgs.legacyPackages.${system});
 
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          nativeBuildInputs = [
             pkgs.nodejs
             pkgs.pnpm
           ];
         };
-      }
-    );
+      });
+    };
 }
